14210eace911c2f95106eba457e51943374c9740
NahueM <lnahuelmaya@gmail.com>
2022-03-22 00:30:53 +0100

    logic improve

---
+++ components/JobDetailContent.tsx
@@ -0,0 +1,27 @@
+import PhraseCard from '../components/shared/PhraseCard'
+import { UseJobsContext } from '../hooks/useJobsContext'
+import { FormattedMessage } from 'react-intl'
+
+function JobDetailContent () {
+  const [state] = UseJobsContext()
+  const { jobSelected } = state
+  const { jobDescription, companyDescription, id } = jobSelected
+
+  return (
+    <div className='flex border-none max-w-4xl flex-col m-5 mt-20 justify-center items-center bg-white md:max-w-[65%] lg:max-w-[70%]'>
+        <PhraseCard />
+        <h1 className='text-center text-3xl font-bold my-10'><FormattedMessage id={id}/></h1>
+        <div className='flex-col p-5 shadow-md items-center border border-light-blue shadow-lg justify-center text-center mb-10 divide-b-2'>
+            <div className='flex items-center justify-center'>
+                <img src={`/${id}.jpg`} className='' />
+            </div>
+            <p className='font-bold text-grey'><FormattedMessage id={companyDescription}/></p>
+        </div>
+        <article className='text-justify text-xl'>
+            <FormattedMessage id={jobDescription}/>
+        </article>
+    </div>
+  )
+}
+
+export default JobDetailContent
--- pages/jobDetail.tsx
+++ pages/jobDetail.tsx
@@ -1,33 +1,27 @@
 import type { NextPage } from 'next'
 import AppLayout from '../components/appLayout'
 import SideBar from '../components/shared/SideBar'
-import PhraseCard from '../components/shared/PhraseCard'
 import { UseJobsContext } from '../hooks/useJobsContext'
-import { FormattedMessage } from 'react-intl'
+import JobDetailContent from '../components/JobDetailContent'
+import Loading from '../components/shared/Loading'
 
 const JobDetail: NextPage = () => {
   const [state] = UseJobsContext()
-  const { jobSelected } = state
-  const { jobDescription, companyDescription, id } = jobSelected
 
   return (
       <AppLayout>
+          {
+            state.jobSelected
+              ? (
             <div className='flex h-full'>
-                <div className='flex border-none max-w-4xl flex-col m-5 mt-20 justify-center items-center bg-white md:max-w-[65%] lg:max-w-[70%]'>
-                    <PhraseCard />
-                    <h1 className='text-center text-3xl font-bold my-10'><FormattedMessage id={id}/></h1>
-                    <div className='flex-col p-5 shadow-md items-center border border-light-blue shadow-lg justify-center text-center mb-10 divide-b-2'>
-                        <div className='flex items-center justify-center'>
-                            <img src={`/${id}.jpg`} className='' />
-                        </div>
-                        <p className='font-bold text-grey'><FormattedMessage id={companyDescription}/></p>
-                    </div>
-                    <article className='text-justify text-xl'>
-                        <FormattedMessage id={jobDescription}/>
-                    </article>
-                </div>
+                <JobDetailContent />
                 <SideBar />
             </div>
+                )
+              : (
+                <Loading />
+                )
+          }
       </AppLayout>
   )
 }
